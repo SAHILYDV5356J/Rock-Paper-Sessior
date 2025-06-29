7b649604f7d3c99e9ca4d735f4449224a87a5ecc
Sahil Yadav <sy05092005@gmail.com>
2025-06-29 18:58:42 +0530

    Update rock.js

--- rock.js
+++ rock.js
@@ -37,7 +37,7 @@ const showwinner=(userwin,userchoice,compchoice)=>{
        
     }
     else{
-        console.log("Computer Win!");
+        console.log("You Lose!");
         compscore++;
         msg.innerText=`You Lose! ${compchoice.toUpperCase()} beats ${userchoice.toUpperCase()}`;
          msg.style.backgroundColor="red";
